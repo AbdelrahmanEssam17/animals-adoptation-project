263acd55431bf0dd52aba5e1e0acef5479345a30
dinaadel555 <ddinaadel952002@gmail.com>
2026-02-22 21:43:57 +0200

    add favourite doctor to user

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ bootstrap(app,express)
 app.listen(process.env.PORT,()=>{
     console.log(`server is running on port 4000`)
 })
+
+import favouriteRoutes from "./src/moduels/favourite/favourite.routes.js"
+
+app.use("/api/favourite", favouriteRoutes);
