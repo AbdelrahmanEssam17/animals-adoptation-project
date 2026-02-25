7210b6e95d7a35e75fbf7b92b7a9829262ac36e7
dinaadel555 <ddinaadel952002@gmail.com>
2026-02-26 00:45:42 +0200

    finish modules 2

--- index.js
+++ index.js
@@ -9,7 +9,3 @@ bootstrap(app,express)
 app.listen(process.env.PORT,()=>{
     console.log(`server is running on port 4000`)
 })
-
-import favouriteRoutes from "./src/moduels/favourite/favourite.routes.js"
-
-app.use("/api/favourite", favouriteRoutes);
